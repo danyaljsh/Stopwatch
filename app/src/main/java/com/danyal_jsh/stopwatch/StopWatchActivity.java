8bd1f5a7e9c9ae02d4e946a6a2837d7d03e47d10
danyaljsh <39697920+danyaljsh@users.noreply.github.com>
2019-12-29 17:40:15 +0330

    complete StopWatch Activity

--- app/src/main/java/com/danyal_jsh/stopwatch/StopWatchActivity.java
+++ app/src/main/java/com/danyal_jsh/stopwatch/StopWatchActivity.java
@@ -1,13 +1,55 @@
 package com.danyal_jsh.stopwatch;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.os.SystemClock;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.Chronometer;
+import android.widget.ImageView;
 
 public class StopWatchActivity extends AppCompatActivity {
+
+  private Button btnStart,btnStop;
+  private ImageView ivAnchor;
+  private Animation ivAnchorAnim;
+  private Chronometer timerHere;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_stop_watch);
+
+    btnStart =findViewById(R.id.btnstart);
+    btnStop =findViewById(R.id.btnstop);
+    ivAnchor =findViewById(R.id.ivanchor);
+    timerHere =findViewById(R.id.timerhere);
+
+    btnStop.setAlpha(0);
+    //load Animation
+    ivAnchorAnim = AnimationUtils.loadAnimation(this,R.anim.anchoranim);
+
+    //load font
+    Typeface mMedium =Typeface.createFromAsset(getAssets(),"font/MMedium.ttf");
+
+    //customize font
+    btnStart.setTypeface(mMedium);
+
+    btnStart.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+       //start Animation
+      ivAnchor.startAnimation(ivAnchorAnim);
+      btnStop.animate().alpha(1).translationY(-160).setDuration(500).start();
+      btnStart.animate().alpha(0).setDuration(500).start();
+
+      //startTime
+        timerHere.setBase(SystemClock.elapsedRealtime());
+        timerHere.start();
+      }
+    });
   }
 }
